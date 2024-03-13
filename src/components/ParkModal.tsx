9cb132be1bf10efc01f2e6718e95da02eaa81fed
tatecarson <tatecarson@gmail.com>
2024-03-13 15:31:28 +0000

    Remove commented out code in ParkModal component

--- src/components/ParkModal.tsx
+++ src/components/ParkModal.tsx
@@ -38,9 +38,9 @@ function ParkModal({ setIsOpen, isOpen, parkName, parkDistance, userOrientation 
                             <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                                     <div className="sm:flex sm:items-start">
-                                        <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                                        {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
                                             <SpeakerWaveIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                                        </div>
+                                        </div> */}
                                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                                             <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
                                                 {parkName}
